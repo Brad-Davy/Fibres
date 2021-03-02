a7a9d431b469ab29850c06dc7077ca3b134b4b12
Brad-Davy <69325766+Brad-Davy@users.noreply.github.com>
2021-03-02 15:58:11 +0000

    Update Fibrous.py

--- Fibrous.py
+++ Fibrous.py
@@ -5,12 +5,15 @@ import bpy
 
 
 
+domain_cross_section = 0.1
+
+
 #distribution = stats.vonmises.pdf(np.linspace(-np.pi,np.pi,2000),Cp)*np.pi/1000
-alpha = 0.05
+alpha = 0.6
 angles = open(bpy.path.abspath("//Cp001.txt"),'r').read().split('\n')
 fibre_diameter = 0.01
 Cp = 0.001
-w = 2
+w = 0.5
 L = 0.2
 length_fibres = 1
 N = (4*alpha*L*w**2)/(length_fibres*np.pi*fibre_diameter**2)
@@ -38,21 +41,38 @@ def create_cylinder(position = [0,0,0], rotation = [0,0,0], scale = [0.01,0.01,0
     so.scale[0] = scale[0]
     so.scale[1] = scale[1]
     so.scale[2] = scale[2]
+    bpy.ops.object.modifier_add(type='BOOLEAN')
+    bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
+    bpy.context.object.modifiers["Boolean"].object =    bpy.data.objects["Cube"]
+    bpy.ops.object.modifier_apply(modifier="Boolean")
     
 
+def create_fibres():
+    for i in range(int(N)):
+       create_cylinder(position = [float(np.random.rand()*L),
+       random.choice((-1, 1))*float(np.random.rand()*w/2),
+       random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
+       scale = [fibre_diameter/2,fibre_diameter/2,length_fibres/2])
+   
+ 
+ 
+def create_cube(): 
+    bpy.ops.mesh.primitive_cube_add()
+    so = bpy.context.active_object
+    so.scale[0] = L
+    so.scale[1] = 2*w
+    so.scale[2] = 2*w
+    so.location[0] = L/2
+    so.location[1] = 0
+    so.location[2] = 0
 
-for i in range(int(N)):
-   create_cylinder(position = [float(np.random.rand()*L),
-   random.choice((-1, 1))*float(np.random.rand()*w/2),
-   random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
-   scale = [fibre_diameter/2,fibre_diameter/2,length_fibres/2])
-   
-   
-#bpy.ops.mesh.primitive_cube_add()
-#so = bpy.context.active_object
-#so.scale[0] = L/2
-#so.scale[1] = w/2
-#so.scale[2] = w/2
-#so.location[0] = L/2
-#so.location[1] = 0
-#so.location[2] = 0
+    bpy.ops.mesh.primitive_cube_add()
+    so = bpy.context.active_object
+    so.scale[0] = L
+    so.scale[1] = domain_cross_section
+    so.scale[2] = domain_cross_section
+    so.location[0] = L/2
+    so.location[1] = 0
+    so.location[2] = 0
+
+create_fibres()

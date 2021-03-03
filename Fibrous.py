a428b21ec06bee94453bb9be181c653d69a9871a
Brad-Davy <69325766+Brad-Davy@users.noreply.github.com>
2021-03-03 13:55:43 +0000

    Update Fibrous.py

--- Fibrous.py
+++ Fibrous.py
@@ -5,31 +5,33 @@ import bpy
 
 
 
-domain_cross_section = 0.1
+domain_cross_section = 200
 
 
-#distribution = stats.vonmises.pdf(np.linspace(-np.pi,np.pi,2000),Cp)*np.pi/1000
-alpha = 0.6
+inner_alpha = 1.5
+outer_alpha = 4
 angles = open(bpy.path.abspath("//Cp001.txt"),'r').read().split('\n')
-fibre_diameter = 0.01
+outer_fibre_diameter = 40
+inner_fibre_diameter = range(3,15,1)
 Cp = 0.001
-w = 0.5
-L = 0.2
-length_fibres = 1
-N = (4*alpha*L*w**2)/(length_fibres*np.pi*fibre_diameter**2)
-print(N)
+w = 800
+L = 40
+L_inner = 100
+length_fibres = 1600
+N = (4*outer_alpha*L*w**2)/(length_fibres*np.pi*outer_fibre_diameter**2)
+N_inner = (4*inner_alpha*L*w**2)/(length_fibres*np.pi*8**2)
+
 
 def create_cylinder(position = [0,0,0], rotation = [0,0,0], scale = [0.01,0.01,0.3]):
     
     bpy.ops.mesh.primitive_cylinder_add()
     so = bpy.context.active_object
-   
-    
+
     ## Define the position of the cylinder ##
 
-    so.location[0] = position[0]
-    so.location[1] = position[1]
-    so.location[2] = position[2]
+    so.location[0] = position[0]/1000
+    so.location[1] = position[1]/1000
+    so.location[2] = position[2]/1000
 
     ## Define the rotation of the cylinder ##
     so.rotation_euler[0] += rotation[0]
@@ -38,41 +40,51 @@ def create_cylinder(position = [0,0,0], rotation = [0,0,0], scale = [0.01,0.01,0
 
 
     ## Define the size of the cylinder ##
-    so.scale[0] = scale[0]
-    so.scale[1] = scale[1]
-    so.scale[2] = scale[2]
+    so.scale[0] = scale[0]/1000
+    so.scale[1] = scale[1]/1000
+    so.scale[2] = scale[2]/1000
     bpy.ops.object.modifier_add(type='BOOLEAN')
     bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
     bpy.context.object.modifiers["Boolean"].object =    bpy.data.objects["Cube"]
     bpy.ops.object.modifier_apply(modifier="Boolean")
     
 
-def create_fibres():
+def create_fibres(pos_off_set = 0):
     for i in range(int(N)):
-       create_cylinder(position = [float(np.random.rand()*L),
+       create_cylinder(position = [float(pos_off_set)+float(np.random.rand()*L),
        random.choice((-1, 1))*float(np.random.rand()*w/2),
        random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
-       scale = [fibre_diameter/2,fibre_diameter/2,length_fibres/2])
+       scale = [outer_fibre_diameter/2,outer_fibre_diameter/2,length_fibres/2])
    
  
- 
+def create_inner_fibres(pos_off_set = 40):
+    for i in range(int(N_inner)):
+       random_num = random.choice(inner_fibre_diameter)
+       create_cylinder(position = [float(pos_off_set)+float(np.random.rand()*L_inner),
+       random.choice((-1, 1))*float(np.random.rand()*w/2),
+       random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
+       scale = [random_num/2,random_num/2,length_fibres/2])
+   
+
 def create_cube(): 
     bpy.ops.mesh.primitive_cube_add()
     so = bpy.context.active_object
-    so.scale[0] = L
-    so.scale[1] = 2*w
-    so.scale[2] = 2*w
-    so.location[0] = L/2
+    so.scale[0] = L/1000
+    so.scale[1] = 8*w/1000
+    so.scale[2] = 8*w/1000
+    so.location[0] = L/2000
     so.location[1] = 0
     so.location[2] = 0
 
     bpy.ops.mesh.primitive_cube_add()
     so = bpy.context.active_object
-    so.scale[0] = L
-    so.scale[1] = domain_cross_section
-    so.scale[2] = domain_cross_section
-    so.location[0] = L/2
+    so.scale[0] = L/1000
+    so.scale[1] = domain_cross_section/1000
+    so.scale[2] = domain_cross_section/1000
+    so.location[0] = L/2000
     so.location[1] = 0
     so.location[2] = 0
 
-create_fibres()
+create_fibres(pos_off_set = 0)
+create_fibres(pos_off_set = 160)
+create_inner_fibres()

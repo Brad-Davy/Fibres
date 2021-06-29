db0c088ac8d2d443e1ab0d0eab895d874deb80e5
Brad-Davy <69325766+Brad-Davy@users.noreply.github.com>
2021-06-29 12:07:16 +0100

    Update Fibrous.py

--- Fibrous.py
+++ Fibrous.py
@@ -1,90 +1,329 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import random 
-import math 
-import bpy
+import os
+
+## Define parameters ##
+length = 2000
+mesh_domain = 200
+inner_packing = 0.2
+outer_packing = 0.4
 
 
+## Open text files to write to ##
+x_1data = open('x_1.txt','w')
+y_1data = open('y_1.txt','w')
+x_2data = open('x_2.txt','w')
+y_2data = open('y_2.txt','w')
+x_3data = open('x_3.txt','w')
+y_3data = open('y_3.txt','w')
+r = open('r.txt','w')
+x_1data.write('')
+y_1data.write('')
+x_2data.write('')
+y_2data.write('')
+x_3data.write('')
+r.write('')
+y_3data.write('')
+x_1data.close()
+y_1data.close()
+x_2data.close()
+y_2data.close()
+x_3data.close()
+y_3data.close()
 
-domain_cross_section = 200
+
+## Detects the intersection between the fibres and the domain and returns the new fibre length##
+def plot_lines(x,y,angle):
+    r1 = [x+(length/2)*np.sin(angle),y+(length/2)*np.cos(angle)]
+    r2 = [x-(length/2)*np.sin(angle),y-(length/2)*np.cos(angle)]
+    m,b = np.polyfit([r1[0],r2[0],x],[r1[1],r2[1],y],1)
 
 
-inner_alpha = 1.5
-outer_alpha = 4
-angles = open(bpy.path.abspath("//Cp001.txt"),'r').read().split('\n')
-outer_fibre_diameter = 40
-inner_fibre_diameter = range(3,15,1)
-Cp = 0.001
-w = 800
-L = 40
-L_inner = 100
-length_fibres = 1600
-N = (4*outer_alpha*L*w**2)/(length_fibres*np.pi*outer_fibre_diameter**2)
-N_inner = (4*inner_alpha*L*w**2)/(length_fibres*np.pi*8**2)
-
-
-def create_cylinder(position = [0,0,0], rotation = [0,0,0], scale = [0.01,0.01,0.3]):
+    plt.plot([r1[0],r2[0]],[r1[1],r2[1]], color = 'black')
+    plt.xlim(-mesh_domain-100,mesh_domain+100)
+    plt.ylim(-mesh_domain-100, mesh_domain+100)
     
-    bpy.ops.mesh.primitive_cylinder_add()
-    so = bpy.context.active_object
+    
+    if angle <= np.radians(45) and (mesh_domain-b)/m < mesh_domain and (-mesh_domain-b)/m > -mesh_domain:
 
-    ## Define the position of the cylinder ##
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+    elif angle <= np.radians(45) and (mesh_domain-b)/m > mesh_domain:
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = mesh_domain
+        y2 = mesh_domain*m + b
+        
+    elif angle <= np.radians(45) and (-mesh_domain-b)/m < -mesh_domain:
+        x1 = -mesh_domain
+        y1 = -mesh_domain*m + b
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+        
+    if np.radians(45) < angle <= np.radians(135) and -mesh_domain*m + b > -mesh_domain and m*mesh_domain + b > -mesh_domain and -mesh_domain*m + b < mesh_domain and m*mesh_domain + b < mesh_domain:
 
-    so.location[0] = position[0]/1000
-    so.location[1] = position[1]/1000
-    so.location[2] = position[2]/1000
+        x1 = mesh_domain
+        y1 = m*mesh_domain + b
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+        
+    elif np.radians(45) < angle <= np.radians(135) and -mesh_domain*m + b < -mesh_domain:
+        
+        x1 = mesh_domain
+        y1 = m*mesh_domain + b
+        
+        
+        x2 = (-mesh_domain-b)/m
+        y2 = -mesh_domain
+        
+    elif np.radians(45) < angle <= np.radians(135) and m*mesh_domain + b < -mesh_domain:
 
-    ## Define the rotation of the cylinder ##
-    so.rotation_euler[0] += rotation[0]
-    so.rotation_euler[1] += rotation[1]
-    so.rotation_euler[2] += rotation[2]
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+        
+    elif np.radians(45) < angle <= np.radians(135) and m*mesh_domain + b > mesh_domain:
+
+        x1 = (mesh_domain-b)/m
+        y1 = mesh_domain
+        
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b    
+        
+    elif np.radians(45) < angle <= np.radians(135) and -mesh_domain*m + b > mesh_domain:
+
+        x1 = (mesh_domain-b)/m
+        y1 = mesh_domain
+        
+        
+        x2 = mesh_domain
+        y2 = mesh_domain*m + b
+        
+    
+    
+    if np.radians(135) < angle < np.radians(225) and (mesh_domain-b)/m > -mesh_domain and (mesh_domain-b)/m < mesh_domain and (-mesh_domain-b)/m < mesh_domain and (-mesh_domain-b)/m > -mesh_domain:
+        print('yes')
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+    elif np.radians(135) < angle < np.radians(225) and (mesh_domain-b)/m > mesh_domain:
+    
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = mesh_domain
+        y2 = mesh_domain*m+b
+        
+    elif np.radians(135) < angle < np.radians(225) and (mesh_domain-b)/m < -mesh_domain:
+    
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m+b
+        
+    elif np.radians(135) < angle < np.radians(225) and (-mesh_domain-b)/m > mesh_domain:
+       
+        x1 = mesh_domain
+        y1 = mesh_domain*m+b
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+    elif np.radians(135) < angle < np.radians(225) and (-mesh_domain-b)/m < -mesh_domain:
+  
+        x1 = -mesh_domain
+        y1 = -mesh_domain*m + b
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+        
+    if np.radians(225) <= angle < np.radians(315)  and mesh_domain > m*mesh_domain + b > -mesh_domain and -mesh_domain*m + b < mesh_domain and -mesh_domain*m + b > -mesh_domain:
+
+        x1 = mesh_domain
+        y1 = m*mesh_domain + b
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+        
+    elif np.radians(225) <= angle < np.radians(315)  and m*mesh_domain + b < -mesh_domain:
+        
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+        
+    elif np.radians(225) <= angle < np.radians(315)  and -mesh_domain*m + b > mesh_domain:
+  
+        x1 = mesh_domain
+        y1 = mesh_domain*m + b
+        
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+    
+    elif np.radians(225) <= angle < np.radians(315)  and -mesh_domain*m + b < -mesh_domain:
+
+        x1 = mesh_domain
+        y1 = mesh_domain*m + b
+        
+        
+        x2 = (-mesh_domain-b)/m
+        y2 = -mesh_domain
+        
+    elif np.radians(225) <= angle < np.radians(315)  and m*mesh_domain + b > mesh_domain:
+
+        x1 = (mesh_domain-b)/m
+        y1 = mesh_domain
+        
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+        
+    if  np.radians(315) <= angle and (-mesh_domain-b)/m < mesh_domain and (mesh_domain-b)/m > -mesh_domain:
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+    elif np.radians(315) <= angle and (-mesh_domain-b)/m > mesh_domain:
+        x1 = mesh_domain
+        y1 = mesh_domain*m + b
+        
+        x2 = (mesh_domain-b)/m
+        y2 = mesh_domain
+        
+    elif np.radians(315) <= angle  and (mesh_domain-b)/m < -mesh_domain:
+        x1 = (-mesh_domain-b)/m
+        y1 = -mesh_domain
+        
+        x2 = -mesh_domain
+        y2 = -mesh_domain*m + b
+    
+        
+    
+    plt.scatter([x1],[y1], color = 'black')
+    plt.scatter(x2,y2, color = 'black')
+    
+    return ((x1-x2)**2 + (y1-y2)**2)**0.5
 
 
-    ## Define the size of the cylinder ##
-    so.scale[0] = scale[0]/1000
-    so.scale[1] = scale[1]/1000
-    so.scale[2] = scale[2]/1000
-    bpy.ops.object.modifier_add(type='BOOLEAN')
-    bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-    bpy.context.object.modifiers["Boolean"].object =    bpy.data.objects["Cube"]
-    bpy.ops.object.modifier_apply(modifier="Boolean")
+## Create filter layer ##
+def create_filter():
+    ## Declaration of variables ##
+    inner_fibre_diameter = range(3,15,1)
+    vol = 0
+    i = 0
+    
+    ## Begin main loop ##
+    while vol < inner_packing:
+        
+        ## Define the total volume and open required text files ##
+        total_volume = 80*400*400
+        x_data = open('x_2.txt','a')
+        y_data = open('y_2.txt','a')
+        r = open('r.txt','a')
+        angles = open("Cp001.txt",'r').read().split('\n')
+        
+        ## Create all random numbers ##
+        random_num = random.choice(inner_fibre_diameter)/2
+        y = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)
+        x = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)   
+        angle = abs(float(angles[i]))
+        
+        ## Write the data to a text file ##
+        x_data.write(str(x)+'\n')
+        y_data.write(str(y)+'\n')
+        r.write(str(random_num)+'\n')
+        r.close()
+        x_data.close()
+        y_data.close()
+        
+        ## Determine the new length and calculate the new volume, 
+        ## count the amount of fibres with variable i
+        new_length = plot_lines(x,y,angle = angle)
+        volume = ((np.pi*(random_num**2))*new_length)/total_volume
+        vol = vol + volume
+        i = i + 1
+
+
+## Creates the ouer fibres ##
+def create_outer():
+    vol = 0
+    i = 0
+    while vol < outer_packing:
+        total_volume = 80*400*400
+        x_data = open('x_3.txt','a')
+        y_data = open('y_3.txt','a')
+        #r = open('r.txt','a')
+        random_num = 20
+        angles = open("Cp001.txt",'r').read().split('\n')
+        y = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)
+        x = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)   
+        x_data.write(str(x)+'\n')
+        y_data.write(str(y)+'\n')
+
+        x_data.close()
+        y_data.close()
+        angle = abs(float(angles[i]))
+        print(x,y,np.degrees(angle))
+        new_length = plot_lines(x,y,angle = angle)
+        print(new_length)
+        volume = ((np.pi*(random_num**2))*new_length)/total_volume
+        vol = vol + volume
+        i = i+1
+        
+## Create the fibres facing the face of the individual ##
+def create_inner():
+    vol = 0
+    i = 0
+    while vol < outer_packing:
+        total_volume = 80*400*400
+        x_data = open('x_1.txt','a')
+        y_data = open('y_1.txt','a')
+        #r = open('r.txt','a')
+        random_num = 20
+        angles = open("Cp001.txt",'r').read().split('\n')
+        y = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)
+        x = random.choice((-1, 1))*float(np.random.rand()*mesh_domain)   
+        x_data.write(str(x)+'\n')
+        y_data.write(str(y)+'\n')
+        x_data.close()
+        y_data.close()
+        angle = abs(float(angles[i]))
+        print(x,y,np.degrees(angle))
+        new_length = plot_lines(x,y,angle = angle)
+        print(new_length)
+        volume = ((np.pi*(random_num**2))*new_length)/total_volume
+        vol = vol + volume
+        i = i+1
+        
+## Create each layer ##
+create_inner()
+create_filter()
+create_outer()
+
+
     
 
-def create_fibres(pos_off_set = 0):
-    for i in range(int(N)):
-       create_cylinder(position = [float(pos_off_set)+float(np.random.rand()*L),
-       random.choice((-1, 1))*float(np.random.rand()*w/2),
-       random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
-       scale = [outer_fibre_diameter/2,outer_fibre_diameter/2,length_fibres/2])
-   
- 
-def create_inner_fibres(pos_off_set = 40):
-    for i in range(int(N_inner)):
-       random_num = random.choice(inner_fibre_diameter)
-       create_cylinder(position = [float(pos_off_set)+float(np.random.rand()*L_inner),
-       random.choice((-1, 1))*float(np.random.rand()*w/2),
-       random.choice((-1, 1))*float(np.random.rand()*w/2)], rotation = [float(angles[i]),0,0], 
-       scale = [random_num/2,random_num/2,length_fibres/2])
-   
 
-def create_cube(): 
-    bpy.ops.mesh.primitive_cube_add()
-    so = bpy.context.active_object
-    so.scale[0] = L/1000
-    so.scale[1] = 8*w/1000
-    so.scale[2] = 8*w/1000
-    so.location[0] = L/2000
-    so.location[1] = 0
-    so.location[2] = 0
-
-    bpy.ops.mesh.primitive_cube_add()
-    so = bpy.context.active_object
-    so.scale[0] = L/1000
-    so.scale[1] = domain_cross_section/1000
-    so.scale[2] = domain_cross_section/1000
-    so.location[0] = L/2000
-    so.location[1] = 0
-    so.location[2] = 0
-
-create_fibres(pos_off_set = 0)
-create_fibres(pos_off_set = 160)
-create_inner_fibres()
